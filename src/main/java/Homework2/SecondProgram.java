01029a65ca5c084e95bde65d692eb51d548d4423
79826 <Igor.burgat96@gmail.com>
2021-11-04 12:25:59 +0500

    add First program

---
+++ src/main/java/Homework2/SecondProgram.java
@@ -0,0 +1,22 @@
+package Homework2;
+
+    public class SecondProgram {
+
+        public static void main (String[] args) {
+
+            int num1 = 12;
+            int num2 = 19;
+
+            int result = num1 + num2;
+            System.out.println("Результат:" + result);
+
+
+            int num3 = 14;
+            int num4 = 20;
+
+            int result2 = num3 * num4;
+            System.out.println("Результат:" + result2);
+        }
+
+
+    }
