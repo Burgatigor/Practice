4c3874a1fbebd6e221966f7bca3d6690e53ad8fd
79826 <Igor.burgat96@gmail.com>
2021-11-04 17:38:36 +0500

    add Homework2

--- src/main/java/Homework2/SecondProgram.java
+++ src/main/java/Homework2/SecondProgram.java
@@ -6,17 +6,13 @@ package Homework2;
 
             int num1 = 12;
             int num2 = 19;
-
             int result = num1 + num2;
             System.out.println("Результат:" + result);
 
-
             int num3 = 14;
             int num4 = 20;
-
             int result2 = num3 * num4;
             System.out.println("Результат:" + result2);
         }
-
 
     }
